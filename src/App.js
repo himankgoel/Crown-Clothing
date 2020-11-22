a3a4105154e7769baa2b7eda78cac1c79320f170
himankgoel <himankgoel12@gmail.com>
2020-11-23 00:02:03 +0530

    Updated menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import { Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <div>
+      <Route exact path="/" component={Homepage} />
+      {/* <Route exact path="/:" component={Homepage} /> */}
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {withRouter} from "react-router-dom";
 import "./menu-item.styles.scss";
 
-export default function MenuItem({title,imageUrl,size}) {
+function MenuItem({title,imageUrl,size,history,linkUrl,match}) {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div className="background-image"  style ={{ backgroundImage : `url(${imageUrl})`}}/>
                     <div className="content">
                         <h1 className="title">{title.toUpperCase()}</h1>
@@ -12,3 +13,4 @@ export default function MenuItem({title,imageUrl,size}) {
                 </div>
     )
 }
+export default withRouter(MenuItem);
