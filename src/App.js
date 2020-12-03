035c8b9416e62dac6d8e01fabe67e099e5da8654
himankgoel <himankgoel12@gmail.com>
2020-12-03 09:55:42 +0530

    Added Shop page,collec-prev and collection-item component & styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import { Route } from "react-router-dom";
 function App() {
   return (
     <div>
       <Route exact path="/" component={Homepage} />
-      {/* <Route exact path="/:" component={Homepage} /> */}
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
