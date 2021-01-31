8eb58e70825b5358c52f15d27f24009afa7a75d9
himankgoel <himankgoel12@gmail.com>
2021-01-31 13:37:35 +0530

    Added firestore and storing of user in db

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,4 @@
 import firebase from "firebase/app";
-
 import "firebase/firestore";
 import "firebase/auth";
 
@@ -12,11 +11,35 @@ const firebaseConfig = {
   appId: "1:58698453825:web:5b046f683c6479a6b887c0",
   measurementId: "G-T06SSZS5L4",
 };
-
-firebase.initializeApp(firebaseConfig);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return; // IF USER SIGNED OUT
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log("Error creating user : ", err);
+    }
+  }
+  return userRef;
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
