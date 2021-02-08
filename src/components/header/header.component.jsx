6e10e5389e54c6820cc885ee725ba8ccd5cddda4
himankgoel <himankgoel12@gmail.com>
2021-02-08 13:54:18 +0530

    Added redux user-actiond and reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
  import "./header.styles.scss";
 import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 import {ReactComponent as Logo } from "./../../assets/crown.svg"
 import {auth} from "../../firebase/firebase.utils";
  const Header = ({currentUser}) => {
@@ -18,4 +19,7 @@ import {auth} from "../../firebase/firebase.utils";
          </div>
      </div>
  }
- export default Header;
+ const mapStateToProps = state => ({
+     currentUser : state.user.currentUser
+ })
+ export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
