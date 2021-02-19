b00f4944f1f0e16d90cc80d2012b8a82bc544332
himankgoel <himankgoel12@gmail.com>
2021-02-19 10:06:55 +0530

    Completed advanced routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "./../../components/preview-collection/preview-collection.component";
-class Shop extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections : SHOP_DATA
-        }
-    }
-    render(){
-        const {collections} = this.state; 
-        return <div className="shop-page">
-            {collections.map(({id,...otherCollectionProps}) => 
-                <CollectionPreview key={id} {...otherCollectionProps}/>
-            )}
+
+import {Route} from "react-router-dom";
+
+import CollectionOverview from "../../components/collection-overview/collection-overview.component";
+import CollectionPage from "../collection/collection.component";
+
+const ShopPage = ({match}) => (
+        <div className="shop-page">
+           <Route exact path={`${match.path}`} component={CollectionOverview}/>
+           <Route path={`${match.path}/:collectionId`} component={CollectionPage}/>
+           
         </div>
-    }
-}
-export default Shop;
+)
+
+export default ShopPage;
