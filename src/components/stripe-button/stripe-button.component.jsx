532612f5c5ef0738d7189f5c4b4829eecd2abbda
himankgoel <himankgoel12@gmail.com>
2021-02-22 09:50:02 +0530

    Added stripe payment

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe  = price*100;
+    const publishableKey = "pk_test_51INVMLAsCib2e8ea8F0vhHaKE6nYiNXMVPTiNJx0n0Ahy0sxujVSiK89jUIetfcWMfWOzyVsu8YsgSrF21to3Br000T2toOCcz"
+
+    const onToken = token => {
+        console.log(token);
+        alert("Payment Successful!")
+    } 
+
+    return( 
+    <StripeCheckout
+        label="Pay Now"
+        name="CRWN CLOTHING Ltd."
+        billingAddress
+        shippingAddress
+        image="https://svgshare.com/i/CUz.svg"
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey = {publishableKey}
+    />
+    )
+}
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,8 @@ import { createStructuredSelector } from "reselect";
 import {selectCartItems,selectCartTotal} from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
  import "./checkout.styles.scss";
 
  const CheckoutPage = ({cartItems,total}) => (
@@ -27,9 +29,14 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
              </div>
          </div>
          {cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)}
-         <div className="total">
-             <span>TOTAL : ${total}</span>
+         <div className="total"><span>TOTAL : ${total}</span></div>
+         <div className="test-warning">
+             *Please use the following test credit card for payments*
+             <br/>
+             4242 4242 4242 4242 - Exp : Any future date - CVV : 123
          </div>
+        <StripeCheckoutButton price={total}/>
+         
      </div>
  )
  const mapStateToProps = createStructuredSelector({
